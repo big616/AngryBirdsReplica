a9d196bb59c88b8884525d0637cf92c143b98d1f
big616 <big616@users.noreply.github.com>
2018-03-31 15:02:26 +0800

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject deathEffect;
+
+	public float health = 4f;
+
+	public static int EnemiesAlive = 0;
+
+	void Start (){
+		EnemiesAlive++;
+	}
+	
+	
+
+	void OnCollisionEnter2D(Collision2D colInfo)
+	{
+		if (colInfo.relativeVelocity.magnitude > health) {
+			Die ();
+		}
+	}
+
+	void Die (){
+		Instantiate (deathEffect, transform.position, Quaternion.identity);
+		EnemiesAlive--;
+		if (EnemiesAlive <= 0)
+			Debug.Log ("Lvl won!");
+			
+		Destroy (gameObject);
+	}
+}
